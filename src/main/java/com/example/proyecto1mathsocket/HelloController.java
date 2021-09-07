0b0acf775d5fcdb0eca74e7df86ecc8fd9b2ffd1
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2021-09-07 16:57:22 -0600

    Logra movimiento del jugador a la casilla de juego

--- src/main/java/com/example/proyecto1mathsocket/HelloController.java
+++ src/main/java/com/example/proyecto1mathsocket/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.proyecto1mathsocket;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Rectangle Cuadro1;
+
+    @FXML
+    private Rectangle Cuadro2;
+
+    @FXML
+    void CambiarPos(MouseEvent event) {
+        double Pos1, Pos2;
+        Pos1 = Cuadro1.getX();
+        Pos2 = Cuadro2.getLayoutX();
+        //Pos2 ++;
+        System.out.println("Posición X del Cuadro 1: " + Pos1);
+        System.out.println("Posición X del Cuadro 1: " + Pos2);
+
+        Cuadro1.setX(Pos2);
+
     }
+
 }
